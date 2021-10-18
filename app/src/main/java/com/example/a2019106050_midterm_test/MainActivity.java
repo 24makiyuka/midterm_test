f608da7cea13128c6dbaae9b105c1858ecc17fd4
24makiyuka <70732444+24makiyuka@users.noreply.github.com>
2021-10-18 13:10:51 +0900

    game기본구성

--- app/src/main/java/com/example/a2019106050_midterm_test/MainActivity.java
+++ app/src/main/java/com/example/a2019106050_midterm_test/MainActivity.java
@@ -12,11 +12,55 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private final Random random = new Random();
 
+    int aipoint=0, yourpoint=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.button1.setOnClickListener(v -> game("바위"));
+        binding.button2.setOnClickListener(v -> game("가위"));
+        binding.button3.setOnClickListener(v -> game("보"));
     }
+    private void game(String yourChoice) {
+        int ai = random.nextInt(3);
+        String aiChoice;
+        switch (ai){
+            case 0:
+                aiChoice = "바위";
+                break;
+            case 1:
+                aiChoice = "가위";
+                break;
+            default:
+                aiChoice = "보";
+        }
+        binding.textView.setText("AI가" + aiChoice +"를 냈습니다");
+        binding.textView2.setText("당신은" + yourChoice +"를 냈습니다");
+
+        String result;
+
+        if(aiChoice.equals(yourChoice)){
+            result = "비겼습니다..";
+        } else if ((aiChoice.equals("바위") && yourChoice.equals("보")) ||
+                (aiChoice.equals("가위") && yourChoice.equals("바위")) ||
+                (aiChoice.equals("보") && yourChoice.equals("가위"))) {
+            result = "축하합니다! 당신이 이겼습니다";
+            yourpoint++;
+        } else {
+            result = "미안합니다ㅜㅜ 당신이 졌습니다";
+            aipoint++;
+        }
+        binding.textView.setText(result);
+        binding.textView4.setText("AI:"+aipoint);
+        binding.textView5.setText("YOU"+yourpoint);
+
+    }
+
+
+
+
+
 }
